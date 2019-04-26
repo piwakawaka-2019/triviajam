3a11d7921721b4768d0ca35c36979a3a0cf9e4ee
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-04-26 15:26:08 +1200

    css styling

---
+++ client/components/header/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu'
+
+class header extends Component {
+  render () {
+    return (
+<div></div>
+ 
+ 
+);
+}
+}
+
+export default header;
